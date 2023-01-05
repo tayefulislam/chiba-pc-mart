f831f318f3526cefafb8e159106ffad6ecb99662
tayefulislam <kazitayefulislam12@gmail.com>
2023-01-05 23:56:23 +0600

    some field added

--- src/Pages/Admin/Dashboard/AddProducts/AddProducts.js
+++ src/Pages/Admin/Dashboard/AddProducts/AddProducts.js
@@ -1,29 +1,95 @@
 import React from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
+import MenuItem from "@mui/material/MenuItem";
+import Autocomplete from "@mui/material/Autocomplete";
 
 const AddProducts = () => {
+  // seletion values from server
+
+  const productTypes = [
+    {
+      value: "laptop",
+      label: "Laptop",
+    },
+    { value: "desktop", label: "Desktop" },
+    { value: "mobile", label: "Mobile" },
+  ];
+
+  // Brands
+
+  // const brand = [
+  //   { value: "", label: "" },
+  //   { value: "", label: "" },
+  //   { value: "", label: "" },
+  //   { value: "", label: "" },
+  // ]
+
+  const brand = [
+    { value: "apple", label: "Apple" },
+    { value: "hp", label: "HP" },
+
+    { value: "macbook", label: "MacBook" },
+  ];
+
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    console.log("hello");
+    const title = e.target.title.value;
+    const productType = e.target.productType.value;
+
+    console.log(title);
+    console.log(productType);
   };
   return (
     <div>
       <form onSubmit={handleSubmit}>
         <Box
-          component="form"
           sx={{
             "& > :not(style)": { m: 1, width: "25ch" },
           }}
           noValidate
           autoComplete="off"
         >
-          <TextField label="Outlined" variant="outlined" />
-          <TextField label="Outlined" variant="outlined" />
+          <div class="form-control w-full max-w-xs">
+            <label class="label">
+              <span class="label-text">Title</span>
+            </label>
+            <input
+              type="text"
+              required
+              placeholder="Title"
+              name="title"
+              class="input input-bordered input-error w-full max-w-xs"
+            />
+            <label class="label"></label>
+          </div>
 
-          <input type="submit" value="Submit" />
+          {/* Product Type */}
+
+          <TextField
+            id="outlined-select-currency"
+            select
+            label="Product Type"
+            defaultValue="laptop"
+            name="productType"
+          >
+            {productTypes.map((option) => (
+              <MenuItem key={option.value} value={option.value}>
+                {option.label}
+              </MenuItem>
+            ))}
+          </TextField>
+
+          <Autocomplete
+            disablePortal
+            options={brand}
+            sx={{ width: 300 }}
+            renderInput={(params) => <TextField {...params} label="Movie" />}
+          />
         </Box>
+
+        <input type="submit" value="Submit" />
       </form>
     </div>
   );

a898b873eb3ee435b93bc7a964d0322534a29c6e
tayefulislam <kazitayefulislam12@gmail.com>
2023-01-11 14:58:01 +0600

    product demo component added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
-import AddProducts from "./Pages/Admin/Dashboard/AddProducts/AddProducts";
+// import AddProducts from "./Pages/Admin/Dashboard/AddProducts/AddProducts";
+import Product from "./Pages/Admin/Dashboard/Product/Product";
 
 function App() {
   return (
     <div className="App">
-      <AddProducts></AddProducts>
+      <Product></Product>
     </div>
   );
 }

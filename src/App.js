ea6a4db4cfb917d1f98ad7db280faeca1116a49a
tayefulislam <kazitayefulislam12@gmail.com>
2023-01-12 00:24:22 +0600

    shop component added

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Cart from "./Pages/Admin/Dashboard/Cart/Cart";
 
@@ -5,7 +6,14 @@ import Products from "./Pages/Admin/Dashboard/Products/Products";
 // import AddProducts from "./Pages/Admin/Dashboard/AddProducts/AddProducts";
 
 function App() {
-  console.log("cart");
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("https://chibapcmartdemo.onrender.com/api/v1/products")
+      .then((res) => res.json())
+      .then((data) => setProducts(data?.result));
+  }, []);
+
   return (
     <div className="App">
       <div className="grid grid-cols-2">
--- src/Pages/Admin/Dashboard/Products/Products.js
+++ src/Pages/Admin/Dashboard/Products/Products.js
@@ -17,7 +17,7 @@ const Products = () => {
 
   return (
     <div>
-      <div className="grid  grid-cols-2 sm:grid md:grid-cols-3 gap-3">
+      <div className="grid  grid-cols-2 sm:grid md:grid-cols-3 gap-3 px-2 mt-3">
         {products.map((product, index) => {
           return <Product product={product} key={index}></Product>;
         })}
